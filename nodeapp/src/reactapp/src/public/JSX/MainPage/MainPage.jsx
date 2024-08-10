fd36398dbbc8d56b1962c40214385dbb09c89639
https://github.com/hycho1/AI_chat_supporter_react_nodejs.git <psh676@naver.com>
2024-08-10 19:49:52 +0900

    parksungho240809_05
    mainpagechange

--- nodeapp/src/reactapp/src/public/JSX/MainPage/MainPage.jsx
+++ nodeapp/src/reactapp/src/public/JSX/MainPage/MainPage.jsx
@@ -20,10 +20,12 @@ export default function MainPage() {
       };
 
     return (
-        <div className="MainPage">
-            <div className="Main">
-                <input type='text' name='nickName' placeholder="닉네임을 입력해주세요." onChange={(e)=>{setNickName(e.target.value)}} onKeyDown={keypress}></input>
-                <button  onClick={handleNickNameSubmit}>입장하기</button>
+        <div className="pullpage">
+            <div className="MainPage">
+                <div className="Main">
+                    <input type='text' name='nickName' placeholder="닉네임 입력" onChange={(e)=>{setNickName(e.target.value)}} onKeyDown={keypress}></input>
+                    <button  onClick={handleNickNameSubmit}>Login</button>
+                </div>
             </div>
         </div>
     );
