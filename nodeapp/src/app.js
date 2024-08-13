e6334d51e7008280f918ba945d93b40a07ba6855
juniyan9 <hjoo111@naver.com>
2024-08-13 18:22:28 +0900

    1822 commit

--- nodeapp/src/app.js
+++ nodeapp/src/app.js
@@ -139,7 +139,7 @@ io.on("connection", (socket) => {
   });
 
   
-  });
+  
 
   socket.on('message', (message, roomName) => {
 
@@ -174,140 +174,133 @@ io.on("connection", (socket) => {
     }
   });
 
-
+});
 
 
 
 export { app, io };
 
 
-/* NodeJS declaration */
+// * NodeJS declaration */
 // import express from 'express';
-// import customLogger from './class/customLogger.js';
+// import customLogger from './class/customLogger.js'
+// import userObj from './class/user.js'
+// import roomObj from './class/room.js'
 // import path from 'path';
 // import cors from 'cors';
 // import * as url from 'url';
-// import { createServer } from 'http';
-// import { Server } from 'socket.io';
 
-// // Create the Express app
-// const app = express();
+// let app = express();
+
 // const logger = new customLogger();
 
-// // Create an HTTP server and attach Socket.IO
-// const httpServer = createServer(app);
+// /* WebSocker declaration */
+// import { createServer } from "http";
+// import { Server } from "socket.io";
+
+
+// const httpServer = createServer();
 // const io = new Server(httpServer, { cors: '*' });
+
+
+// /* NodeJS implementation */
 
 // // Set filePath
 // const __filename = url.fileURLToPath(import.meta.url);
 // const __dirname = url.fileURLToPath(new URL('.', import.meta.url));
 
-// // Middleware
-// app.use('/', express.static(path.join(__dirname, 'reactapp/build')));
+// /* middleware */
+// //app.use('요청 경로', express.static('실제 경로'));
+// //app.use('/', express.static(path.join(__dirname, 'reactapp/build')));
+// app.use('/', express.static(__dirname));
+// //app.use(express.static(path.join(__dirname, 'reactapp/build/static')));
+// // parse requests of content-type - application/json
 // app.use(express.json());
+// // parse requests of content-type - application/x-www-form-urlencoded
 // app.use(express.urlencoded({ extended: true }));
-// app.use(cors());
 
-// // Serve the main page
+// app.use(cors({ cors: '*' }));
+
+
+
+
 // app.get('/', (req, res) => {
-//   res.sendFile(path.join(__dirname, 'reactapp/build/index.html'));
+//   res.readFile("index.html")
 // });
 
-// // Sample room data
-// let rooms = [
-//   {
-//     id: 0,
-//     name: 'room1',
-//     count: 1,
-//     maxCount: 10,
-//     password: '1111',
-//   },
-//   {
-//     id: 1,
-//     name: 'room2',
-//     count: 3,
-//     maxCount: 7,
-//     password: '2222',
-//   },
-// ];
+// /*
+  
+// */
 
-// // Endpoint to get the list of rooms
+// let room =[
+//     { 
+//       id : 0,
+//       name: 'room1',  
+//       count: 1,
+//       maxCount: 10,
+//     },
+//     { 
+//       id : 1,
+//       name: 'room2',  
+//       count: 3,
+//       maxCount: 7,
+//     }
+//   ];
+// let users = {};
+
+
+
 // app.get('/chatList', (req, res) => {
-//   res.send(rooms);
+//   res.send(room);
 // });
 
-// /* WebSocket implementation */
 
-// const WS_PORT = 5050;
-// httpServer.listen(WS_PORT, () => {
-//   console.log(`WebSocket listening at port ${WS_PORT}`);
+
+// /* WebSocker implementation */
+
+// const WS_port = 5050;
+// httpServer.listen(WS_port, () => {
+//   console.log('WebSocket listening at port %d', WS_port);  
 // });
 
-// const PORT = 3000;
-// const roomUserCounts = {}; // To track user count per room
-// const users = {}; // To map socket ID to user data
-// const nickNames = new Set(); // To ensure unique nicknames
+// //events
+// io.on("connection", (socket) => {
+//   logger.info('Client connected');
 
-// // WebSocket events
-// io.on('connection', (socket) => {
-//   logger.info('A Client has connected');
+//   socket.on('enter_room', (nickName, roomName) => {
 
-//   // Event for entering a room
-//   socket.on('enter_room', (nickName, roomName, cb = () => {}) => {
-//     if (nickNames.has(nickName)) {
-//       cb({ error: '이미 사용중인 닉네임입니다.' });
-//       return;
-//     }
+//     users[socket.id] = {nickName, roomName};
 
-//     nickNames.add(nickName);
-//     users[socket.id] = { nickName, roomName };
-
+//     logger.info('UserName >>'+ users[socket.id].nickName);
+    
 //     socket.join(roomName);
-
-//     if (!roomUserCounts[roomName]) {
-//       roomUserCounts[roomName] = 0;
-//     }
-
-//     roomUserCounts[roomName] += 1;
-
-//     logger.info(`${nickName} has joined ${roomName}`);
-//     socket.emit('reply', `${nickName}님이 입장하셨습니다. 반갑습니다.`, '관리자봇');
-//     socket.broadcast.to(roomName).emit('reply', `${nickName}님이 입장하셨습니다. 반갑습니다.`, '관리자봇');
+//     logger.info(users[socket.id].nickName + 'Client joined to ' + roomName);
+    
+//     socket.in(roomName).emit("reply", users[socket.id].nickName, "welcome");
 //   });
-
-//   // Event for sending a message
-//   socket.on('message', (message, roomName) => {
-//     logger.info(`Received message from client: ${message}`);
-//     logger.info(`Received message from room: ${roomName}`);
+  
+//   socket.on('message', (message, roomName) => { 
+//     logger.info('Received message from client:' + message);
+//     logger.info('Received message from client room:' + roomName);
 
 //     const user = users[socket.id];
 //     if (user) {
-//       socket.to(roomName).emit('reply', message, user.nickName);
+//       socket.to(roomName).emit('reply', message ,users[socket.id].nickName, message);
 //     }
 //   });
 
-//   // Event for disconnecting
-//   socket.on('disconnect', () => {
+//   socket.on('disconnect', (roomName) => {
 //     const user = users[socket.id];
 //     if (user) {
-//       logger.info('A client has disconnected');
-
-//       const roomName = user.roomName;
-//       if (roomUserCounts[roomName]) {
-//         roomUserCounts[roomName] -= 1;
-//       }
-
-//       logger.info(`${roomName}에 남아있는 사람 수는 ${roomUserCounts[roomName] || 0} 명입니다.`);
-
-//       io.to(roomName).emit('message', `(${user.nickName}) 님이 방을 나가셨습니다.`);
-//       socket.broadcast.to(roomName).emit('message', `(${user.nickName}) 님이 방을 나가셨습니다.`);
-
-//       nickNames.delete(user.nickName);
-//       delete users[socket.id];
+//         logger.info('A client disconnected');
+//         io.to(roomName).emit('message', `${user.nickName} has left the room`);
+//         delete users[socket.id];
 //     }
 //   });
+
 // });
 
-// export { app, io };
+
+// export {app, io};
 
 
