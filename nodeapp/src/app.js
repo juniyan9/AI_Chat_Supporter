38c114cb0f7654620cf6ca231a1bfe3a77d5eefb
112park <psh676@gmail.com>
2024-09-04 18:39:43 +0900

    asd

--- nodeapp/src/app.js
+++ nodeapp/src/app.js
@@ -49,7 +49,7 @@ app.use(session(sessionObj));
 // app.use(cors({
 //   origin: true, // 출처 허용 옵션
 //   credential: true // 사용자 인증이 필요한 리소스(쿠키 ..등) 접근
-// }));
+// })); 
 app.use(cookieParser());
 // app.use(cors({ cors: "*" }));
 const corsOptions = {
