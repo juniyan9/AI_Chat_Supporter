ba60852165cebf583123f0acd4986c04d80adfb8
112park <psh676@gmail.com>
2024-09-06 16:13:40 +0900

    sungho240906_1
    search기능완성

--- nodeapp/src/reactapp/src/public/JSX/ChatPage/Message.jsx
+++ nodeapp/src/reactapp/src/public/JSX/ChatPage/Message.jsx
@@ -1,23 +1,32 @@
 import '../../CSS/Message.css';
 
 export default function Message({nickName, text, user1,highlight}) {
-
-
-  
-  
-    const highligttext =text.split(highlight);
-    // console.log(highligttext);
     
-    // const test =highligttext.lastIndexOf();
-    // console.log(test);
+    const highlighttext = (text, highlight) => {
+      let result = [];
+      let count = 0;
+      let index;      
+  
+      while ((index = text.indexOf(highlight, count)) !== -1) {
+        if (index > count) {
+          result.push(<span key={count}>{text.slice(count, index)}</span>);
+        }
+        result.push(<span key={index} className='highlighttext'>{highlight}</span>);
+  
+        count = index + highlight.length;
+      }
+      if (count < text.length) {
+        result.push(<span key={count}>{text.slice(count)}</span>);
+      }
+      console.log(result);
+      return result;
+    };
     
-
-
     return (
       <div className={user1 ? "user1" : "user2"}>
         <div className={user1 ? "username1" : "username2"}>{nickName}</div>
         <p>
-          {highlight!=('') ? (<div>{highligttext[0]}<span className='highligttext'>{highlight}</span>{highligttext[1]}</div>) : (<div>{text}</div>)}
+          {highlight !== '' ? highlighttext(text, highlight) : (<div>{text}</div>)}
         </p>
       </div>
     )
