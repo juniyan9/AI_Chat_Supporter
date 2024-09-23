b1dcb7817814a2c081286951de8f1fdb7c90da9c
112park <psh676@gmail.com>
2024-09-23 16:11:46 +0900

    psh20240923_2
    infobar충돌방지

--- nodeapp/src/reactapp/src/public/JSX/ChatPage/InfoBar.jsx
+++ nodeapp/src/reactapp/src/public/JSX/ChatPage/InfoBar.jsx
@@ -1,9 +1,9 @@
 import '../../CSS/InfoBar.css';
-import {useState} from 'react';
+import {useState, useEffect} from 'react';
 import userIcon from '../../IMG/multiple_user_icon.png';
 // import { io } from 'socket.io-client';
 
-export default function InfoBar({ roomName, setonsearchtext, roomCount }) {
+export default function InfoBar({ roomName, setonsearchtext, roomCount, maxCount }) {
     const [search, setSearch] = useState('');
 
     const inputText = (e) => {
@@ -11,12 +11,16 @@ export default function InfoBar({ roomName, setonsearchtext, roomCount }) {
         setSearch(value);
         setonsearchtext(value);
     };
+    useEffect (()=> {
+        // roomCount 변화 감지
+        console.log(`Room 인원수 업데이트 : ${roomCount}`)
+    }, [roomCount]); // roomCount가 변경될 때마다 이 useEffect 실행
 
     return (
         <div className="InfoBar">
             <h3>
                 {roomName}&emsp;
-                <img src={userIcon} alt="User Icon" className='userIcon' /> {roomCount}
+                <img src={userIcon} alt="User Icon" className='userIcon' /> {roomCount}/{maxCount}
             </h3>
             <input
                 className='searchtext'
